9d29671572c1386c9eef76eb41f5ce2a193faf24
Nguyen Dac Thang [thangnd] <thangnd@nexondv.com>
2022-10-26 16:45:32 +0700

    config i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import i18next from 'i18next';
-import { I18nextProvider } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { lngs, t } from './languages/i18n';
+
 function App() {
+  const { i18n } = useTranslation();
   return (
     <Router>
-      <I18nextProvider i18n={i18next}>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      </I18nextProvider>
+      <div className='App'>
+        <header className='App-header'>
+          <div>
+            {Object.keys(lngs).map(lng => (
+              <button
+                key={lng}
+                style={{
+                  fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal',
+                }}
+                type='submit'
+                onClick={() => {
+                  i18n.changeLanguage(lng);
+                }}
+              >
+                {lngs[lng as keyof typeof lngs].nativeName}
+              </button>
+            ))}
+          </div>
+
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          ></a>
+        </header>
+        <div>{t(`common.title`)}</div>
+      </div>
     </Router>
   );
 }
 
 export default App;
+``;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import i18n from '@/languages/i18n';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+);
